ea36925db61844d6d2d3d7b9c5be5b720388b645
Yi Ding <83038743+yi-ding-cs@users.noreply.github.com>
2021-11-01 11:28:43 +0800

    Update eeg_dataset.py
    Removed the unused code in eeg_dataset.py

--- code/eeg_dataset.py
+++ code/eeg_dataset.py
@@ -23,19 +23,3 @@ class eegDataset(Dataset):
 
     def __len__(self):
         return len(self.y)
-
-class tripleEEGDataset(Dataset):
-    # x_tensor: (sample, channel, datapoint(feature)) type = torch.tensor
-    # y_tensor: (sample,) type = torch.tensor
-
-    def __init__(self, x_tensor, y_tensor):
-        self.x = x_tensor
-        self.y = y_tensor
-
-        assert self.x.size(0) == self.y.size(0)
-
-    def __getitem__(self, index):
-        return self.x[index], self.y[index]
-
-    def __len__(self):
-        return len(self.y)
