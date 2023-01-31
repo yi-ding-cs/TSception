1412b73a1859ee49a5e556e430530d54794a7432
Yi Ding <83038743+yi-ding-cs@users.noreply.github.com>
2023-01-31 12:18:40 +0800

    Update networks.py

--- code/networks.py
+++ code/networks.py
@@ -56,4 +56,78 @@ class TSception(nn.Module):
         out = self.fc(out)
         return out
 
+class TSceptionIJCNN(nn.Module):
+    """
+    Y. Ding et al., "TSception:A Deep Learning Framework for Emotion Detection Using EEG,"
+    2020 International Joint Conference on Neural Networks (IJCNN), Glasgow, UK, 2020, pp. 1-7,
+    doi: 10.1109/IJCNN48605.2020.9206750.
+    """
+    def conv_block(self, in_chan, out_chan, kernel, step, pool):
+        return nn.Sequential(
+            nn.Conv2d(in_channels=in_chan, out_channels=out_chan,
+                      kernel_size=kernel, stride=step, padding=0),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(kernel_size=(1, pool), stride=(1, pool)))
+
+    def __init__(self, num_classes, input_size, sampling_rate, num_T, num_S, hidden, dropout_rate):
+        # input_size: 1 x EEG channel x datapoint
+        super(TSceptionIJCNN, self).__init__()
+        self.inception_window = [0.5, 0.25, 0.125]
+        self.pool = 8
+        # by setting the convolutional kernel being (1,lenght) and the strids being 1 we can use conv2d to
+        # achieve the 1d convolution operation
+        self.Tception1 = self.conv_block(1, num_T, (1, int(self.inception_window[0] * sampling_rate)), 1, self.pool)
+        self.Tception2 = self.conv_block(1, num_T, (1, int(self.inception_window[1] * sampling_rate)), 1, self.pool)
+        self.Tception3 = self.conv_block(1, num_T, (1, int(self.inception_window[2] * sampling_rate)), 1, self.pool)
+
+        self.Sception1 = self.conv_block(num_T, num_S, (int(input_size[-2]), 1), 1, int(self.pool*0.25))
+        self.Sception2 = self.conv_block(num_T, num_S, (int(input_size[-2] * 0.5), 1), (int(input_size[-2] * 0.5), 1),
+                                         int(self.pool*0.25))
+        self.BN_t = nn.BatchNorm2d(num_T)
+        self.BN_s = nn.BatchNorm2d(num_S)
+
+        size = self.get_size(input_size)
+        self.fc = nn.Sequential(
+            nn.Linear(size[1], hidden),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden, num_classes)
+        )
+
+    def forward(self, x):
+        y = self.Tception1(x)
+        out = y
+        y = self.Tception2(x)
+        out = torch.cat((out, y), dim=-1)
+        y = self.Tception3(x)
+        out = torch.cat((out, y), dim=-1)
+        out = self.BN_t(out)
+        z = self.Sception1(out)
+        out_ = z
+        z = self.Sception2(out)
+        out_ = torch.cat((out_, z), dim=2)
+        out = self.BN_s(out_)
+        out = out.view(out.size()[0], -1)
+        out = self.fc(out)
+        return out
+
+    def get_size(self, input_size):
+        # here we use an array with the shape being
+        # (1(mini-batch),1(convolutional channel),EEG channel,time data point)
+        # to simulate the input data and get the output size
+        data = torch.ones((1, 1, input_size[-2], int(input_size[-1])))
+        y = self.Tception1(data)
+        out = y
+        y = self.Tception2(data)
+        out = torch.cat((out, y), dim=-1)
+        y = self.Tception3(data)
+        out = torch.cat((out, y), dim=-1)
+        out = self.BN_t(out)
+        z = self.Sception1(out)
+        out_final = z
+        z = self.Sception2(out)
+        out_final = torch.cat((out_final, z), dim=2)
+        out = self.BN_s(out_final)
+        out = out.view(out.size()[0], -1)
+        return out.size()
 
